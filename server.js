fad10c643ec058f74309e877aab2ad5231cf828e
William <akhfnml@gmail.com>
2025-07-13 01:07:10 +0800

    Server Port Change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
 
 const events = [];
 
